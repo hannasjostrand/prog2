07b0cdd6be95e611ad8272c8b4086cb905e99cab
Hanna Sjostrand <hasj4147@cronstedt.it.uu.se>
2021-10-18 00:31:55 +0200

    ändrade integer.py

--- integer.py
+++ integer.py
@@ -1,4 +1,4 @@
-""" Python interface to the C++ Integer class """
+""" Phyton interface to the C++ Integer class """
 import ctypes
 lib = ctypes.cdll.LoadLibrary('./libinteger.so')
 
@@ -6,10 +6,12 @@ class Integer(object):
 	def __init__(self, val):
 		lib.Integer_new.argtypes = [ctypes.c_int]
 		lib.Integer_new.restype = ctypes.c_void_p
-		lib.Integer_get.argtypes = [ctypes.c_void_p]
+		lib.Integer_get.argtypes =[ctypes.c_void_p]
 		lib.Integer_get.restype = ctypes.c_int
-		lib.Integer_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
+		lib.Integer_set.argtypes = [ctypes.c_void_p, ctypes.c_int]
 		lib.Integer_delete.argtypes = [ctypes.c_void_p]
+		lib.Integer_fib.argtypes = [ctypes.c_void_p]
+		lib.Integer_fib.restypes = ctypes.c_int
 		self.obj = lib.Integer_new(val)
 
 	def get(self):
@@ -17,6 +19,9 @@ class Integer(object):
 
 	def set(self, val):
 		lib.Integer_set(self.obj, val)
-        
+
 	def __del__(self):
 		return lib.Integer_delete(self.obj)
+
+	def fib(self):
+		return lib.Integer_fib(self.obj)
