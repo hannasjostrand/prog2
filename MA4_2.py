e756e2fbb505a5ddb5cd61a35ee8b81ac08488e4
Hanna Sjostrand <hasj4147@cronstedt.it.uu.se>
2021-10-18 00:30:13 +0200

    ändrade MA4_2-py

--- MA4_2.py
+++ MA4_2.py
@@ -1,12 +1,42 @@
 #!/usr/bin/env python3
-
+import matplotlib.pyplot as plt
 from integer import Integer
+from time import perf_counter as pc
 
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return(fib_py(n-1)+fib_py(n-2))
 def main():
-	f = Integer(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
+#C++
+	f = Integer(47)
+	print(f.fib())
+	#f.set(7)
+	#print(f.fib())
 
-if __name__ == '__main__':
+#python
+	#print(fib_py(6))
+
+#tidtagning
+	a = 30
+	py = []
+	inte = []
+	while a < 45:
+		startpy = pc()
+		fib_py(a)
+		endpy = pc()
+		py.append(endpy-startpy)
+		f = Integer(a)
+		startit = pc()
+		f.fib()
+		endit = pc()
+		inte.append(endit-startit)
+		a += 1
+
+	x = list(range(30,45))
+	plt.plot(x,py,'r',x,inte,'b')
+	plt.savefig('plott.png')
+
+if __name__=='__main__':
 	main()
