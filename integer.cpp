bf834f51aa7fb47e0bce4b84866814a16151856f
Hanna Sjostrand <hasj4147@cronstedt.it.uu.se>
2021-10-18 00:32:17 +0200

    ändrade integer.cpp

--- integer.cpp
+++ integer.cpp
@@ -6,18 +6,31 @@ class Integer{
 		Integer(int);
 		int get();
 		void set(int);
+                int fib();
 	private:
 		int val;
+                int fib1(int);
 	};
- 
+
 Integer::Integer(int n){
 	val = n;
 	}
- 
+
 int Integer::get(){
-	return val;
+        return val;
 	}
- 
+
+int Integer::fib(){
+        return fib1(val);
+	}
+
+int Integer::fib1(int n){
+	if (n <= 1)
+		return n;
+	else
+		return fib1(n-1)+fib1(n-2);
+	}
+
 void Integer::set(int n){
 	val = n;
 	}
@@ -26,6 +39,7 @@ void Integer::set(int n){
 extern "C"{
 	Integer* Integer_new(int n) {return new Integer(n);}
 	int Integer_get(Integer* integer) {return integer->get();}
+        int Integer_fib(Integer* integer) {return integer->fib();}
 	void Integer_set(Integer* integer, int n) {integer->set(n);}
 	void Integer_delete(Integer* integer){
 		if (integer){
